6c60478ef149041c4a926ec90a0a79da00abbbb8
Sanskarchougale <sanskarchougale773@gmail.com>
2025-04-01 21:55:56 +0530

    Add files via upload
    Added Demo.java to the repository

---
+++ Demo.java
@@ -0,0 +1,7 @@
+//Program to demonstrate simple Java program  
+Public class P1 { 
+Public static void main (String [ ] args) { 
+//TODO Auto-generated method stub 
+System.out. println(“Java”); 
+} 
+}
