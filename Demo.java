fdac2e94b2df75aa78f0e335cfae1fa2ee9e11c3
Sanskarchougale <sanskarchougale773@gmail.com>
2025-04-28 10:34:05 +0530

    change string

--- Demo.java
+++ Demo.java
@@ -1,7 +1,6 @@
 //Program to demonstrate simple Java program  
 Public class P1 { 
-Public static void main (String [ ] args) { 
-//TODO Auto-generated method stub 
-System.out. println(“Hello Java”); 
-} 
+	Public static void main (String [ ] args) { 
+		System.out.println(“Hello world”); 
+	} 
 }
